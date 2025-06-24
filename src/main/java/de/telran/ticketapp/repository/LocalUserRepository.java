e17f7a38e19645b6925ef5a760dd7fd4c0974471
Vladiradi <rzhvlad@gmail.com>
2025-06-25 01:48:18 +0200

    done repository

--- src/main/java/de/telran/ticketapp/repository/LocalUserRepository.java
+++ src/main/java/de/telran/ticketapp/repository/LocalUserRepository.java
@@ -1,9 +1,9 @@
 package de.telran.ticketapp.repository;
+
 import de.telran.ticketapp.entity.LocalUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface LocalUserRepository extends JpaRepository<LocalUser, Long> {
-        Optional<LocalUser> findByUsername(String username);
         Optional<LocalUser> findByEmail(String email);
 }
--- src/main/java/de/telran/ticketapp/repository/TicketRepository.java
+++ src/main/java/de/telran/ticketapp/repository/TicketRepository.java
@@ -1,18 +1,24 @@
 package de.telran.ticketapp.repository;
-import java.util.List;
+
 import de.telran.ticketapp.entity.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
+
+    // стандартные методы
     Ticket findByTitle(String title);
+
     List<Ticket> findAllByTitle(String title);
 
-    @Query (value = "SELECT * FROM Ticket t LEFT JOIN LocalUser u ON u.id = t.id")
-    List<Ticket> getAllWithUsers();
-
-    @Query (value = "SELECT t.* FROM ticket as t", nativeQuery = true)
-    List<Ticket> getAllWithAllUsers();
+    // корректный native SQL-запрос
+    @Query(value = """
+        SELECT t.* FROM tickets t 
+        LEFT JOIN local_users u ON t.local_user_id = u.id
+        """, nativeQuery = true)
+    List<Ticket> findAllWithUsers();
 }
